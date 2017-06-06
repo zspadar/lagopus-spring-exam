959f9c339861fbf713251d25e18b70a4e9352f46
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-06-06 13:29:37 +0200

    init(RandomAnswer class)

--- zspadar/src/main/java/com/greenfox/exam/spring/zspadar/model/Randomanswer.java
+++ zspadar/src/main/java/com/greenfox/exam/spring/zspadar/model/Randomanswer.java
@@ -1,6 +1,9 @@
 package com.greenfox.exam.spring.zspadar.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,11 +12,23 @@ import lombok.Setter;
  * Created by zsuzsanna.padar on 2017. 06. 06..
  */
 @Entity
-@Table(name = "randomAnswere")
+@Table(name = "randomAnswer")
 @Getter
 @Setter
-public class Randomanswer {
+public class RandomAnswer {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
+  private String answer;
 
+  public RandomAnswer() {
+  }
+
+  public RandomAnswer(Long id, String answer) {
+    this.id = id;
+    this.answer = answer;
+  }
+  
 }
