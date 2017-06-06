4340c4a89b6d46d6a34e234c27e4102c60c598f7
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-06-06 09:48:42 +0200

    init(Question class)

---
+++ zspadar/src/main/java/com/greenfox/exam/spring/zspadar/model/Question.java
@@ -0,0 +1,28 @@
+package com.greenfox.exam.spring.zspadar.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 06. 06..
+ */
+@Entity
+@Table(name = "Questions")
+@Getter
+@Setter
+public class Question {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String question;
+
+  public Question() {
+  }
+
+}
