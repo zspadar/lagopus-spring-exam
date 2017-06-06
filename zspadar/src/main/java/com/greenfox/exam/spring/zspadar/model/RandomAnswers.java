d8206f19476d83d4e5c14d1c19558ea512d21702
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-06-06 13:31:20 +0200

    init(RandomAswerRepository interface

---
+++ zspadar/src/main/java/com/greenfox/exam/spring/zspadar/model/RandomAnswers.java
@@ -0,0 +1,8 @@
+package com.greenfox.exam.spring.zspadar.model;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 06. 06..
+ */
+public class RandomAnswers {
+
+}
--- zspadar/src/main/java/com/greenfox/exam/spring/zspadar/model/Randomanswer.java
+++ zspadar/src/main/java/com/greenfox/exam/spring/zspadar/model/Randomanswer.java
@@ -30,5 +30,5 @@ public class RandomAnswer {
     this.id = id;
     this.answer = answer;
   }
-  
+
 }
